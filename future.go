90e38a8caa1d1dca33aff91f00051ca26f03ad26
Denis Novikov <denis.novikov@uprove.io>
2022-04-26 18:06:07 +0300

    Removed defer stuff.

--- future.go
+++ future.go
@@ -6,26 +6,16 @@ import (
 	"github.com/denisss025/go-async/pipeline"
 )
 
-var _ = context.Background()
-
 type futureResult[T any] struct {
 	Val T
 	Err error
 }
 
 type Future[T any] struct {
-	deferFn <-chan func()
-	result  <-chan futureResult[T]
+	result <-chan futureResult[T]
 }
 
 func (f *Future[T]) Await(ctx context.Context) (v T, err error) {
-	defer func() {
-		if f.deferFn != nil {
-			fn := <-f.deferFn
-			fn()
-		}
-	}()
-
 	select {
 	case <-ctx.Done():
 		err = ctx.Err()
@@ -38,101 +28,26 @@ func (f *Future[T]) Await(ctx context.Context) (v T, err error) {
 
 func Exec[T any](ctx context.Context, fn func(context.Context) (T, error)) (
 	future *Future[T]) {
-	var executed bool
-
 	return &Future[T]{
-		result: pipeline.Generate(ctx,
-			func(_ context.Context) (r futureResult[T], ok bool) {
-				if ok = !executed; ok {
-					r.Val, r.Err = fn(ctx)
-				}
+		result: pipeline.Map(ctx,
+			func(ctx context.Context) (v futureResult[T]) {
+				v.Val, v.Err = fn(ctx)
 
-				return r, ok
-			}),
-	}
-}
-
-func ExecWithDefer[T any](ctx context.Context,
-	fn func(context.Context) (func(), T, error)) *Future[T] {
-	var executed bool
-
-	dfn := make(chan func())
-
-	return &Future[T]{
-		deferFn: dfn,
-		result: pipeline.Generate(ctx,
-			func(ctx context.Context) (r futureResult[T], ok bool) {
-				if ok = !executed; !ok {
-					return r, ok
-				}
-
-				var deferFn func()
-
-				deferFn, r.Val, r.Err = fn(ctx)
-
-				go func(out chan<- func(), fn func()) {
-					defer close(out)
-
-					if deferFn != nil {
-						out <- fn
-					} else {
-						out <- func() {}
-					}
-				}(dfn, deferFn)
-
-				return r, ok
-			}),
+				return v
+			}, pipeline.ToChan(ctx, ctx)),
 	}
 }
 
 func Then[T, V any](ctx context.Context, first *Future[T],
 	next func(context.Context, T) (V, error)) *Future[V] {
-	deferFn := first.deferFn
-	first.deferFn = nil
-
-	future := Exec(ctx, func(ctx context.Context) (v V, err error) {
-		t, err := first.Await(ctx)
-		if err == nil {
-			v, err = next(ctx, t)
-		}
-
-		return v, err
-	})
-
-	if deferFn != nil {
-		future.deferFn = deferFn
-	}
-
-	return future
-}
-
-func ThenWithDefer[T, V any](ctx context.Context, first *Future[T],
-	next func(context.Context, T) (func(), V, error)) *Future[V] {
-	deferFn := first.deferFn
-	first.deferFn = nil
-
-	future := ExecWithDefer(ctx,
-		func(ctx context.Context) (dfn func(), v V, err error) {
-			t, err := first.Await(ctx)
-			if err == nil {
-				dfn, v, err = next(ctx, t)
-			}
-
-			return func() {
-				if dfn != nil {
-					dfn()
+	return &Future[V]{
+		result: pipeline.Map(ctx,
+			func(t futureResult[T]) (v futureResult[V]) {
+				if v.Err = t.Err; v.Err == nil {
+					v.Val, v.Err = next(ctx, t.Val)
 				}
 
-				if deferFn != nil {
-					dfn = <-deferFn
-					dfn()
-				}
-			}, v, err
-		})
-
-	if deferFn != nil {
-		future.deferFn = deferFn
+				return v
+			}, first.result),
 	}
-
-	return future
 }
--- future_test.go
+++ future_test.go
@@ -1,1 +1,60 @@
-package async
+package async_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	. "github.com/denisss025/go-async"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExec(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	t.Run("with delay", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancel := context.WithTimeout(ctx, time.Second)
+
+		defer cancel()
+
+		getFive := Exec(ctx, func(ctx context.Context) (int, error) {
+			select {
+			case <-ctx.Done():
+				return 0, ctx.Err()
+			case <-time.After(time.Microsecond):
+				return 5, nil
+			}
+		})
+
+		five, err := getFive.Await(ctx)
+
+		assert.NoError(t, err)
+		assert.Equal(t, 5, five)
+	})
+
+	t.Run("with timeout", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancel := context.WithTimeout(ctx, time.Microsecond)
+
+		defer cancel()
+
+		getFive := Exec(ctx, func(ctx context.Context) (int, error) {
+			select {
+			case <-ctx.Done():
+				return 5, ctx.Err()
+			case <-time.After(time.Second):
+				return 5, nil
+			}
+		})
+
+		zero, err := getFive.Await(ctx)
+
+		assert.Error(t, err)
+		assert.Zero(t, zero)
+	})
+}
