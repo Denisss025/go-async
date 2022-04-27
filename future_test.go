dc71b6cac5ef2e6b2c784b605c6ba3379b8f0922
Denis Novikov <denis.novikov@uprove.io>
2022-04-27 16:28:36 +0300

    Tests for Then
     Added test cases for Then function.

--- future_test.go
+++ future_test.go
@@ -2,6 +2,7 @@ package async_test
 
 import (
 	"context"
+	"io"
 	"testing"
 	"time"
 
@@ -56,5 +57,59 @@ func TestExec(t *testing.T) {
 
 		assert.Error(t, err)
 		assert.Zero(t, zero)
+	})
+}
+
+func TestThen(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	t.Run("without error", func(t *testing.T) {
+		t.Parallel()
+
+		getFive := Exec(ctx, func(_ context.Context) (int, error) {
+			return 5, nil
+		})
+
+		makeSlice := Then(ctx, getFive,
+			func(_ context.Context, n int) ([]struct{}, error) {
+				return make([]struct{}, n), nil
+			})
+
+		slice, err := makeSlice.Await(ctx)
+		assert.NoError(t, err)
+		assert.Len(t, slice, 5)
+	})
+
+	t.Run("Future.Then", func(t *testing.T) {
+		t.Parallel()
+
+		ten, err := Exec(ctx, func(_ context.Context) (int, error) {
+			return 5, nil
+		}).Then(ctx, func(_ context.Context, i int) (int, error) {
+			return i * 2, nil
+		}).Await(ctx)
+
+		assert.NoError(t, err)
+		assert.Equal(t, 10, ten)
+	})
+
+	t.Run("error", func(t *testing.T) {
+		t.Parallel()
+
+		var called bool
+
+		zero, err := Exec(ctx, func(_ context.Context) (int, error) {
+			return 5, io.EOF
+		}).Then(ctx, func(_ context.Context, i int) (int, error) {
+			called = true
+
+			return i * 2, nil
+		}).Await(ctx)
+
+		assert.Error(t, err)
+		assert.Zero(t, zero)
+		assert.False(t, called)
 	})
 }
